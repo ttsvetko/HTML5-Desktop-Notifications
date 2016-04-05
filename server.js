065fcb7406941949b606d34455f128866f6469ba
Tsvetan Tsvetkov <ttsvetkov@intracol.com>
2016-04-05 17:05:04 +0300

    Add demo html page(not ready yet) and update server.js file

--- server.js
+++ server.js
@@ -15,7 +15,18 @@
  *
  * Author: Tsvetan Tsvetkov (tsekach@gmail.com)
  */
-var connect = require('connect');
-connect.createServer(
-    connect.static(__dirname)
-).listen(8080);
+var finalhandler = require('finalhandler');
+var http = require('http');
+var serveStatic = require('serve-static');
+
+// Serve up public/ftp folder
+var serve = serveStatic('.', {'index': ['demo/index.html']});
+
+// Create server
+var server = http.createServer(function(req, res){
+    var done = finalhandler(req, res);
+    serve(req, res, done);
+});
+
+// Listen
+server.listen(8080);
