c500fb4340363b065d61289b506ad3e9bfab1ba3
Tsvetan Tsvetkov <tsekach@gmail.com>
2016-09-12 15:27:17 +0300

    Add deployment

--- grunt/aliases.js
+++ grunt/aliases.js
@@ -6,7 +6,6 @@ module.exports = {
     // Default Grunt task that builds the Deep blue theme ready for production
     'default': [
         'clean',
-        'jasmine',
         'uglify:default',
         'uglify:dev'
     ],
@@ -14,7 +13,6 @@ module.exports = {
     // Development Grunt build task
     'dev': [
         'clean',
-        'jasmine',
         'watch'
     ]
 };
